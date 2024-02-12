2dd8a2292eaaf2b057de0f168a3c3f17c187fef4
KornAlexander <92106145+KornAlexander@users.noreply.github.com>
2024-02-12 11:02:21 +0100

    Update Download All Items in Power BI Report Server.ps1

--- PBIRS/Download All Items in Power BI Report Server.ps1
+++ PBIRS/Download All Items in Power BI Report Server.ps1
@@ -1,4 +1,4 @@
-﻿Out-RsCatalogItem	 -ReportServerUri 'http://dashboard.fanuc.local/ReportServer' -Path / -Recurse -Destination C:\Users\92900054\Desktop\SSRSDownload
+﻿Out-RsCatalogItem	 -ReportServerUri 'http://dashboard.fanuc.local/ReportServer' -Path / -Recurse -Destination C:\Users\UNSERNAME\Desktop\SSRSDownload
 
 $PSVersionTable
 
